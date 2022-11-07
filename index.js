f0cc4b9551fb192a582caee083e41935a9de70fd
mahabubr <mahabub.r753@gmail.com>
2022-11-08 01:19:05 +0600

    service home get method implement

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const port = process.env.PORT || 5000
+require('dotenv').config()
 require('colors');
 
 // Middle Wares
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vlhy1ml.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+
+        const serviceCollection = client.db('AccountServices').collection('Service')
+
+        // Service Server
+
+        app.get('/home-services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const result = await cursor.limit(3).toArray()
+            res.send(result)
+        })
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+run()
 
 app.get('/', (req, res) => {
     res.send('Server Running')
